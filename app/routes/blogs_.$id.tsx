638bc058a09bfbd6e16c43584c4f23f0f121c3c0
Sujal <web.sujal@gmail.com>
2024-08-05 16:50:40 +0530

    feat: updated ui for each blog page

--- app/routes/blogs_.$id.tsx
+++ app/routes/blogs_.$id.tsx
@@ -1,11 +1,16 @@
 import { readItem } from "@directus/sdk";
+import { Box, Button, Group, Image, Stack } from "@mantine/core";
 import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  Link,
+  useLoaderData,
+  useOutletContext,
+  useSubmit,
+} from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import BlogCard from "~/components/BlogCard/BlogCard";
 import directus from "~/lib/directus.server";
-import { Blog } from "~/types";
+import { Blog, User } from "~/types";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const id = params.id;
@@ -27,8 +32,38 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 const BlogPost = () => {
   const blog = useLoaderData<typeof loader>() as Blog;
+  const user = useOutletContext<User>();
 
-  return <BlogCard blog={blog} />;
+  const submit = useSubmit();
+
+  const handleDelete = () => {
+    const response = confirm("Please confirm you want to delete this task.");
+
+    if (response) {
+      submit(null, { method: "delete", action: `/delete/${blog?.id}` });
+    }
+  };
+
+  return (
+    <Stack py={20}>
+      <Image src={blog.image_url} alt={blog.title} />
+
+      <Box dangerouslySetInnerHTML={{ __html: blog.content }} />
+
+      {/* edit and delete blog */}
+      {user.userId === blog.author && (
+        <Group gap="lg">
+          <Button component={Link} to={`/blogs/edit/${blog.id}`}>
+            Edit
+          </Button>
+
+          <Button type="button" onClick={handleDelete} bg="red">
+            Delete
+          </Button>
+        </Group>
+      )}
+    </Stack>
+  );
 };
 
 export default BlogPost;
